b9aeacf361155e3e2041e6c00c17a71de9329e78
ioannsls <ioann@DESKTOP-SBLCQ9V.localdomain>
2021-12-20 04:35:12 +0300

    client changes rolled back

--- 20_2.c
+++ 20_2.c
@@ -7,17 +7,10 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
-
-#define BUF_SIZE 512
+ 
 #define SHM_NAME "/clock"
 #define TIMESTR_SIZE 64
 volatile int g_terminate = 0;
-
-struct shmbuf {
-	sem_t  sem1;            /* POSIX unnamed semaphore */
-	size_t cnt;             /* Number of bytes used in 'buf' */
-	char   buf[BUF_SIZE];   /* Data being transferred */
-};
  
 struct shared_buffer_t {
     unsigned write_flag;
@@ -49,39 +42,32 @@ int main() {
         }
     }
  
-    struct shmbuf *shmp = mmap(
+    void* p = mmap(
         NULL,
         sysconf(_SC_PAGE_SIZE),
         PROT_READ | PROT_WRITE,
         MAP_SHARED,
         shm_des,
         0);
-    if (shmp == MAP_FAILED) {
+    if (p == MAP_FAILED) {
         perror("Failed to map shared memory");
         close(shm_des);
         unlink(SHM_NAME);
         return 3;
     }
     close(shm_des);
-    printf("Allocated page at [%p]\n", shmp);
+    printf("Allocated page at [%p]\n", p);
         
-    struct shared_buffer_t* buf = (struct shared_buffer_t*)shmp;
+    struct shared_buffer_t* buf = (struct shared_buffer_t*)p;
     const char* time_str = (const char*)buf->string;
     unsigned int read_flag;
     while(!g_terminate) {
         read_flag = buf->write_flag;
-	if (sem_wait(&shmp->sem1) == -1) {
-		perror("sem init");
-		sem_destroy(&shmp->sem1);
-		return 6;
-	}
-	//добавить семафор, который проверяет не добавляет ли что-то сервер
         printf("[%.*s]\n", TIMESTR_SIZE, time_str);
         if (read_flag != buf->write_flag)
             continue;
         sleep(1);
     }
     printf("\nStopping client...\n");
-    sem_destroy(&shmp->sem1);
     return 0;
 }
